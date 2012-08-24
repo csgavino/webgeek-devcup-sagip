1e3ad2b90fe9f8ab9b54d6ea535f205c66f82ef8
Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>
2012-08-24 14:18:13 +0800

    Initial
    Signed-off-by: Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebGeek::Application.config.secret_token = 'e20983b5bc455e4d8937b5c668833f9d8f2a4c0d8abdae4b6cbc021bbb74098de1e94f8d0d84f135a8333e7cec3bc3305b269cf5780c4cf976a7bcf42fcc4a40'
