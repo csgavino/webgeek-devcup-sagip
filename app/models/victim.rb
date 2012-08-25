c33d77178cb50bf00b7efdfd47beea475d43dd63
Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>
2012-08-25 16:58:25 +0800

    Validate victim creation
    Signed-off-by: Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>

--- app/models/victim.rb
+++ app/models/victim.rb
@@ -3,6 +3,11 @@ class Victim < ActiveRecord::Base
   belongs_to :user
   attr_accessible :age, :description, :name, :shelter_id, :status, :user_id
 
+  validates_presence_of :name, :maximum => 255
+
+  validates_presence_of :age
+  validates_numericality_of :age, :greater_than => 0, :less_than => 120, :only_integer => true
+
   CRITICAL = 1
   SICK  = 2
   ALIVE = 3
@@ -10,4 +15,5 @@ class Victim < ActiveRecord::Base
   scope :name_like, lambda { |victim_name|
     { :conditions => ["name ~* ?", victim_name] }
   }
+
 end
