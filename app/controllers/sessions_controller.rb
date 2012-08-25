c253ffd0ca984e65e85b8c1b1a20ae77d74fc5ba
Kimson Wong <kimson.wong@teamcodeflux.com>
2012-08-25 15:57:19 +0800

    Specified alert message types

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,16 +7,18 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, :notice => "Logged in!"
+      flash[:success] = "You are now signed in!"
+      redirect_to root_url
     else
-      flash.now.alert = "Invalid email or password"
+      flash.now[:danger] = "Invalid email or password"
       render "new"
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, :notice => "Logged out!"
+    flash[:info] = "Logged out!"
+    redirect_to root_url
   end
 
 end
