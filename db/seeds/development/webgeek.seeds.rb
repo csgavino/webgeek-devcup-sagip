8a03ce8e82739f08a024fd5edfe86ee0fb142b2e
Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>
2012-08-25 19:05:25 +0800

    Created new seeds
    Signed-off-by: Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>

--- db/seeds/development/webgeek.seeds.rb
+++ db/seeds/development/webgeek.seeds.rb
@@ -1,8 +1,8 @@
 user1 = User.create(:name => "Super Volunteer",:email => "test@test.com", :password => "password", :password_confirmation => "password")
 user2 = User.create(:name => "Ultra Volunteer",:email => "webgeek@test.com", :password => "password", :password_confirmation => "password")
 
-shelter1 = Shelter.create(:name => "Shelter One", :address => "Address One", :latitude => "", :longitude => "", :capacity => 1000)
-shelter2 = Shelter.create(:name => "Shelter Two", :address => "Address Two", :latitude => "", :longitude => "", :capacity => 2000)
+shelter1 = Shelter.create(:name => "Malanday Elementary School", :address => "Malanday Elementary School, Malaya Street, Marikina City, Metro Manila, Philippines", :latitude => 14.6502489, :longitude => 121.0943609, :capacity => 1000)
+shelter2 = Shelter.create(:name => "Nangka Elementary School", :address => "Nangka Elementary School, Marikina City, Metro Manila, Philippines", :latitude => 14.673237, :longitude => 121.1083958, :capacity => 2000)
 
 
 Victim.create(:name => "Juan Dela Cruz", :age => 34, :status => 1, :description => "Middle-Aged Man", :shelter_id => shelter1.id, :user_id => user1.id)
