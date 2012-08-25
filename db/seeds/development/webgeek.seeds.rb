77390cf1617c9703d36dae623a34cb0c80404309
Jess Malvin Chin <jess@teamcodeflux.com>
2012-08-25 17:53:36 +0800

    Changed statuses for the victim
    Signed-off-by: Jess Malvin Chin <jess@teamcodeflux.com>

--- db/seeds/development/webgeek.seeds.rb
+++ db/seeds/development/webgeek.seeds.rb
@@ -6,6 +6,6 @@ shelter2 = Shelter.create(:name => "Shelter Two", :address => "Address Two", :la
 
 
 Victim.create(:name => "Juan Dela Cruz", :age => 34, :status => 1, :description => "Middle-Aged Man", :shelter_id => shelter1.id, :user_id => user1.id)
-Victim.create(:name => "Martin Florante Baja", :age => 21, :status => 1, :description => "Young Professional Man", :shelter_id => shelter1.id, :user_id => user1.id)
-Victim.create(:name => "Juan Dela Cruz", :age => 40, :status => 1, :description => "Middle-Aged Man With A Similar Name", :shelter_id => shelter2.id, :user_id => user2.id)
-Victim.create(:name => "Marton Gunad Bajardo", :age => 56, :status => 1, :description => "Old-Aged Man", :shelter_id => shelter2.id, :user_id => user2.id)
+Victim.create(:name => "Martin Florante Baja", :age => 21, :status => 2, :description => "Young Professional Man", :shelter_id => shelter1.id, :user_id => user1.id)
+Victim.create(:name => "Juan Dela Cruz", :age => 40, :status => 3, :description => "Middle-Aged Man With A Similar Name", :shelter_id => shelter2.id, :user_id => user2.id)
+Victim.create(:name => "Marton Gunad Bajardo", :age => 56, :status => 4, :description => "Old-Aged Man", :shelter_id => shelter2.id, :user_id => user2.id)
